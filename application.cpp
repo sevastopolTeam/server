4cf348cb17b056fe29be21cf963a023abc924736
jon2205 <jon2205@rambler.ru>
2019-10-21 18:14:19 +0300

    rework dir

--- application.cpp
+++ application.cpp
@@ -1,9 +1,9 @@
 #include "application.h"
 
 #include <iostream>
-#include <stdio.h>
-#include <string>
-#include <string.h>
+
+#include <handlers/hi.h>
+#include <handlers/numbers.h>
 
 TApplication::TApplication() {
     std::cout << "Starting server..." << std::endl;
@@ -11,50 +11,11 @@ TApplication::TApplication() {
     Client = mongoc_client_new("mongodb://localhost:1235");
     Collection = mongoc_client_get_collection(Client, "prod", "base");
 
-    Server.Get("/hi",               [&](const httplib::Request& req, httplib::Response& res){ this->HiHandler(req, res);}); 
-    Server.Get(R"(/numbers/(\d+))", [&](const httplib::Request& req, httplib::Response& res){ this->NumbersHandler(req, res);});
+    Server.Get("/hi",               [&](const httplib::Request& req, httplib::Response& res){ HiHandler(Collection, req, res);});
+    Server.Get(R"(/numbers/(\d+))", [&](const httplib::Request& req, httplib::Response& res){ NumbersHandler(Collection, req, res);});
 
     std::cout << "Server started" << std::endl;
     Server.listen("0.0.0.0", 1234);
-}
-
-void TApplication::HiHandler(const httplib::Request& req, httplib::Response& res) {
-    const bson_t* doc;
-    bson_t* query = bson_new();
-    mongoc_cursor_t* cursor = mongoc_collection_find_with_opts(Collection, query, NULL, NULL);
-
-    std::string ans;
-    while (mongoc_cursor_next(cursor, &doc)) {
-        char* str = bson_as_canonical_extended_json(doc, NULL);
-        printf("%s\n", str);
-        if (!ans.empty()) {
-            ans += ",";
-        }   
-        ans += std::string(str);
-        bson_free(str);
-    }
-
-    bson_destroy(query);
-    mongoc_cursor_destroy(cursor);
-    ans = "[" + ans + "]";
-    res.set_content(ans, "text/plain");
-}
-
-void TApplication::NumbersHandler(const httplib::Request& req, httplib::Response& res) {
-    auto numbers = req.matches[1];
-    res.set_content(numbers, "text/plain");
-    bson_t* doc = bson_new();
-    bson_oid_t oid;
-    bson_oid_init(&oid, NULL);
-    BSON_APPEND_OID(doc, "_id", &oid);
-    BSON_APPEND_UTF8(doc, "hello", std::string(numbers).c_str());
-
-    bson_error_t error;
-    if (!mongoc_collection_insert(Collection, MONGOC_INSERT_NONE, doc, NULL, &error)) {
-        printf("%s\n", error.message);
-    }   
-
-    bson_destroy(doc);
 }
 
 TApplication::~TApplication() {
--- application.h
+++ application.h
@@ -9,9 +9,6 @@ public:
 
     ~TApplication();
 private:
-    void HiHandler(const httplib::Request& req, httplib::Response& res);
-    void NumbersHandler(const httplib::Request& req, httplib::Response& res);
-
     httplib::Server Server;
     mongoc_client_t* Client;
     mongoc_collection_t* Collection;
