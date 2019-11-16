4da7bfde8170f4cf3fcde083e0f464a6e097d18d
AlexBurkov <alexburkov93@gmail.com>
2019-11-16 17:28:45 +0300

    Replace iostream to our defines (#44)

--- sources/handlers/numbers.h
+++ sources/handlers/numbers.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <iostream>
-
 #include "contrib/httplib/httplib.h"
 #include "sources/data_source/data_source.h"
+
+#include "util/generic/iostream.h"
 
 void NumbersHandler(TDataSource& dataSource, const httplib::Request& req, httplib::Response& res) {
 	auto numbers = req.matches[1];
@@ -12,6 +12,6 @@ void NumbersHandler(TDataSource& dataSource, const httplib::Request& req, httpli
     NJson::TJsonValue json;
     json["hello"] = TString(numbers);
     if (!dataSource.CollectionBase.Insert(json)) {
-        std::cout << "error insert" << std::endl;
+        Cout << "error insert" << Endl;
     }
 }
---
+++ util/generic/iostream.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+
+#define Cout std::cout
+#define Cin std::cin
+#define Endl std::endl
