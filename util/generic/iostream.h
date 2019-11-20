1b3c0336538152965537317377e8d0dec151bb5d
Antonov Zhenya <jon2205@rambler.ru>
2019-11-20 13:15:55 +0300

    fix commit

--- util/generic/iostream.h
+++ util/generic/iostream.h
@@ -2,25 +2,37 @@
 
 #include <ctime>
 #include <iostream>
+#include <fstream>
+
+#include "holder.h"
+#include "string.h"
 
 #define Cout std::cout
 #define Cin std::cin
 #define Endl std::endl
 
 namespace {
-    static time_t seconds;
-    static struct tm* timeinfo;
-    static char buffer[20];
+    static time_t _seconds;
+    static struct tm* _local;
+    static char _buffer[20];
 
-    inline char* GetNowTime() {
-        seconds = time(NULL);
-        timeinfo = localtime(&seconds);
-        strftime(buffer, 20, "%Y/%m/%dT%X", timeinfo);
-        return buffer;
+    inline char* _GetNowTime() {
+        _seconds = time(NULL);
+        _local = localtime(&_seconds);
+        strftime(_buffer, 20, "%Y/%m/%dT%X", _local);
+        return _buffer;
     }
 } // namespace
 
-#define OUTPUT_DATETIME GetNowTime()
+#define OUTPUT_DATETIME _GetNowTime()
 
-#define INFO_LOG Cout << "[INFO] " << OUTPUT_DATETIME << ": "
-#define ERROR_LOG Cout << "[ERROR] " << OUTPUT_DATETIME << " (" << __FILE__ << ":" << __LINE__ <<"): "
+static std::ostream* _CoutInfo(&Cout);
+/*
+static void DoInitGlobalLog(const TString& logPath) {
+    if (logPath != "console") {
+        _CoutInfo = new std::ofstream(logPath);
+    }
+}
+*/
+#define ERROR_LOG *_CoutInfo << "[ERROR] " << OUTPUT_DATETIME << " (" << __FILE__ << ":" << __LINE__ <<"): "
+#define INFO_LOG *_CoutInfo << "[INFO] " << OUTPUT_DATETIME << ": "
