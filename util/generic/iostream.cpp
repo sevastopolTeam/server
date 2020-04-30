4429acc232947388f3ff46170212333de00c5937
Antonov Zhenya <jon2205@rambler.ru>
2020-04-30 16:40:14 +0300

    fix

--- util/generic/iostream.cpp
+++ util/generic/iostream.cpp
@@ -3,7 +3,7 @@
 std::ostream* _CoutLog(&Cout);
 
 void DoInitGlobalLog(const TString& logPath) {
-    if (logPath == "preimka") {
+    if (logPath == "priemka") {
         _CoutLog = new std::ofstream("logs/preimka.log", std::ios_base::out);
     } else if (logPath != "console") {
         _CoutLog = new std::ofstream(logPath, std::ios_base::app);
