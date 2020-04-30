2a07bda922c4827b238da3ea2674652622899a5d
Antonov Zhenya <jon2205@rambler.ru>
2020-04-30 16:38:05 +0300

    rewrite file for tests

--- tests/run_test.bat
+++ tests/run_test.bat
@@ -1,6 +1,7 @@
 echo >/dev/null # >nul & GOTO WINDOWS & rem ^
 echo 'Processing for Linux'
 
+mkdir logs
 ./server config/priemka.json &
 mongo mongodb://localhost:1235/priemka tests/prepare_db_to_tests.js
 sleep 3
@@ -14,6 +15,7 @@ exit 0
 :WINDOWS
 echo "Processing for Windows"
 
+mkdir logs
 start /B Debug\server.exe config\priemka.json
 mongo mongodb://localhost:1235/priemka tests/prepare_db_to_tests.js
 sleep 5
--- util/generic/iostream.cpp
+++ util/generic/iostream.cpp
@@ -3,7 +3,9 @@
 std::ostream* _CoutLog(&Cout);
 
 void DoInitGlobalLog(const TString& logPath) {
-    if (logPath != "console") {
+    if (logPath == "preimka") {
+        _CoutLog = new std::ofstream("logs/preimka.log", std::ios_base::out);
+    } else if (logPath != "console") {
         _CoutLog = new std::ofstream(logPath, std::ios_base::app);
     }
 }
