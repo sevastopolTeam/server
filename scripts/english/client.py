1ec01b2ad54a53b31e68cfbbd210a4bf50965af3
AlexBurkov <buriksurik@mail.ru>
2020-05-08 01:39:08 +0300

    R 118 (#119)
    * Move client to scripts
    
    * Add popular words for uploading
    
    * fix bad merge
    
    * Add translations to initial data

--- scripts/english/client.py
+++ scripts/english/client.py
@@ -3,8 +3,6 @@ import json
 import random
 from data_generator import Fake
 
-API_URL = "http://localhost:5050/api/"
-
 PATH_TO_REGISTER_USER = "english/users"
 PATH_TO_LOGIN_USER = "english/login"
 PATH_TO_LOGOUT = "english/logout"
@@ -13,7 +11,9 @@ PATH_TO_ADMIN_TRANSLATIONS = "english/admin/translations"
 
 class Client:
 
-    @classmethod
+    def __init__(self, api_url):
+        self.api_url = api_url
+
     def get_request(self, url, data, headers = dict()):
         url_with_params = url + "?"
         for key in data:
@@ -24,7 +24,6 @@ class Client:
         else:
             return [False, r.status_code]
 
-    @classmethod
     def post_request(self, url, data, headers = dict()):
         r = requests.post(url, data=json.dumps(data), headers=headers)
         if r.status_code == 200:
@@ -32,7 +31,6 @@ class Client:
         else:
             return [False, r.status_code]
 
-    @classmethod
     def put_request(self, url, data, headers = dict()):
         r = requests.put(url, data=json.dumps(data), headers=headers)
         if r.status_code == 200:
@@ -40,7 +38,6 @@ class Client:
         else:
             return [False, r.status_code]
 
-    @classmethod
     def delete_request(self, url, data, headers = dict()):
         r = requests.delete(url, data=json.dumps(data), headers=headers)
         if r.status_code == 200:
@@ -48,48 +45,38 @@ class Client:
         else:
             return [False, r.status_code]
 
-    @classmethod
     def registered_headers(self):
         user = Fake.user()
-        Client.register_user(user)
-        response = Client.login_user(user)[1]
+        self.register_user(user)
+        response = self.login_user(user)[1]
 
         return { "Authorization": response["Body"]["SessionToken"] }
 
-    @classmethod
     def admin_headers(self):
-        response = Client.login_user({"Email": "admin@admin.ru", "Password": "admin"})[1]
+        response = self.login_user({"Email": "admin@admin.ru", "Password": "admin"})[1]
         return { "Authorization": response["Body"]["SessionToken"] }
 
-    @classmethod
-    def filter_dict(self, input, keys):
-        return { key: input.get(key) for key in keys }
-
-    @classmethod
     def register_user(self, user):
         return self.post_request(
-            API_URL + PATH_TO_REGISTER_USER,
+            self.api_url + PATH_TO_REGISTER_USER,
             user
         )
 
-    @classmethod
     def login_user(self, user):
         return self.post_request(
-            API_URL + PATH_TO_LOGIN_USER,
+            self.api_url + PATH_TO_LOGIN_USER,
             user
         )
 
-    @classmethod
     def logout(self, user):
         return self.post_request(
-            API_URL + PATH_TO_LOGIN_USER,
+            self.api_url + PATH_TO_LOGIN_USER,
             user
         )
 
-    @classmethod
     def login_as_admin(self):
         res = self.post_request(
-            API_URL + PATH_TO_LOGIN_USER,
+            self.api_url + PATH_TO_LOGIN_USER,
             {
                 "Email": "admin@admin.ru",
                 "Password": "admin"
@@ -98,47 +85,36 @@ class Client:
 
         return res[1]["Body"]["SessionToken"]
 
-    @classmethod
     def logout(self, token):
-        return self.delete_request(API_URL + PATH_TO_LOGOUT, {})
+        return self.delete_request(self.api_url + PATH_TO_LOGOUT, {})
 
-
-    @classmethod
     def get_translation_by_id(self, translation, headers = None):
         if headers == None:
             headers = self.admin_headers()
-        return self.get_request(API_URL + PATH_TO_ADMIN_TRANSLATIONS + "/" + translation["Id"], {}, headers)
+        return self.get_request(self.api_url + PATH_TO_ADMIN_TRANSLATIONS + "/" + translation["Id"], {}, headers)
 
-    @classmethod
     def get_translations(self, params = {}, headers = None):
         if headers == None:
             headers = self.admin_headers()
-        return self.get_request(API_URL + PATH_TO_ADMIN_TRANSLATIONS, params, headers)
+        return self.get_request(self.api_url + PATH_TO_ADMIN_TRANSLATIONS, params, headers)
 
-    @classmethod
     def delete_translation(self, translation, headers = None):
         if headers == None:
             headers = self.admin_headers()
-        return self.delete_request(API_URL + PATH_TO_ADMIN_TRANSLATIONS + "/" + translation["Id"], {}, headers)
+        return self.delete_request(self.api_url + PATH_TO_ADMIN_TRANSLATIONS + "/" + translation["Id"], {}, headers)
 
-    @classmethod
     def create_translation(self, translation, headers = None):
         if headers == None:
             headers = self.admin_headers()
-        return self.post_request(API_URL + PATH_TO_ADMIN_TRANSLATIONS, translation, headers)
+        return self.post_request(self.api_url + PATH_TO_ADMIN_TRANSLATIONS, translation, headers)
 
-    @classmethod
     def edit_translation(self, translation, headers = None):
         if headers == None:
             headers = self.admin_headers()
-        return self.put_request(API_URL + PATH_TO_ADMIN_TRANSLATIONS, translation, headers)
+        return self.put_request(self.api_url + PATH_TO_ADMIN_TRANSLATIONS, translation, headers)
 
-    @classmethod
     def clear_translations(self):
-        _, response = self.get_request(API_URL + PATH_TO_ADMIN_TRANSLATIONS, {}, self.admin_headers())
+        _, response = self.get_request(self.api_url + PATH_TO_ADMIN_TRANSLATIONS, {}, self.admin_headers())
         if response["Body"]["Records"]:
             for translation in response["Body"]["Records"]:
                 self.delete_translation(translation)
-
-
-
